17c6e86e7b0e8e4135ab6f6aa99f66c4e8e4f0a7
zach.puckett@nintex.com <zach@dokuforest.com>
2017-09-28 14:21:10 -0700

    init commit

---
+++ cmd.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+set -e
+
+### begin login
+loginCmd='az login -u "$loginId" -p "$loginSecret"'
+
+# handle opts
+if [ "$loginTenantId" != " " ]; then
+    loginCmd=$(printf "%s --tenant %s" "$loginCmd" "$loginTenantId")
+fi
+
+case "$loginType" in
+    "user")
+        echo "logging in as user"
+        ;;
+    "sp")
+        echo "logging in as service principal"
+        loginCmd=$(printf "%s --service-principal" "$loginCmd")
+        ;;
+esac
+eval "$loginCmd" >/dev/null
+
+echo "setting default subscription"
+az account set --subscription "$subscriptionId"
+### end login
+
+instrumentationKey=$(az resource show \
+    --namespace "microsoft.insights" \
+    --resource-type "components" \
+    --resource-group "$resourceGroup" \
+    --name "$name" \
+    --query "properties.InstrumentationKey" \
+    --output tsv)
+
+echo "instrumentationKey=$instrumentationKey"
